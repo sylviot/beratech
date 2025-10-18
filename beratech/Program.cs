e49dc6cd91157514a558d7c636e9f342af9cf3b3
sjunior <sjunior@sendit.pt>
2025-10-18 20:37:09 +0100

    init

---
+++ beratech/Program.cs
@@ -0,0 +1,29 @@
+namespace beratech
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+
+            app.Run();
+        }
+    }
+}
